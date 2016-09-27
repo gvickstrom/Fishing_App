3537fbebf51b37768cf1aaa9151b5f1e8bca1c75
Adam Franzen <adamfranzen@gmail.com>
2016-09-26 20:05:31 -0600

    temp knex query fixed in index.js for /single-river

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -24,23 +24,29 @@ router.get('/sign-up', function (req, res, next) {
 router.get('/single-river', function (req, res, next) {
 
   const renderObject = {};
+  var latitude;
+  var longitude;
 
-  axios.get('https://api.darksky.net/forecast/2e41cd367153b0382dd154001a4576fc/39.2541,-105.2276')
-  .then(function (weatherPayload) {
-    renderObject.weatherPayload = weatherPayload.data;
-    knex('reports')
-    .where('station_id', 1)
-    .select()
-    .then((reports) => {
-      renderObject.reports = reports;
-      console.log(renderObject.reports);
+  knex('users')
+  .join('reports', 'user_id', 'users.id')
+  .join('stations', 'station_id', 'stations.id')
+  .where('station_id', 1)
+  .select()
+  .then((reports) => {
+    renderObject.reports = reports;
+    latitude = reports[0].lat;
+    longitude = reports[0].lon;
+    console.log(reports[0].lat,reports[0].lon);
+    axios.get(`https://api.darksky.net/forecast/2e41cd367153b0382dd154001a4576fc/${reports[0].lat},${reports[0].lon}`)
+    .then(function (weatherPayload) {
+      renderObject.weatherPayload = weatherPayload.data;
       res.render('single-river', renderObject);
     })
+    .catch(function (error) {
+      console.log(error);
+    });
   })
-  .catch(function (error) {
-    console.log(error);
-  });
-
 });
+
 
 module.exports = router;
