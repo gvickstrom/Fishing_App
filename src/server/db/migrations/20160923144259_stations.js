486fb8148904ce92a5c1b492f9681ad3b0ed8900
gvickstrom <gvickstrom@gmail.com>
2016-09-27 15:05:00 -0600

    can make api call to usgs and update database

--- src/server/db/migrations/20160923144259_stations.js
+++ src/server/db/migrations/20160923144259_stations.js
@@ -4,9 +4,7 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.varchar('river');
     table.varchar('site_name');
-    table.decimal('water_temp');
     table.decimal('flow_rate');
-    table.decimal('depth');
     table.varchar('reading_date_time');
     table.varchar('lat');
     table.varchar('lon');
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const queries = require('../db/queries');
 const axios = require('axios');
 const knex = require('../db/knex.js');
+const request = require('request');
 
 const indexController = require('../controllers/index');
 
@@ -46,26 +47,35 @@ router.get('/single-river', function (req, res, next) {
 
 router.get('/usgs', function (req, res, next) {
   const renderObject = {};
-  axios.get('http://waterservices.usgs.gov/nwis/iv/?format=json&sites=07079300,07081200,07083710,07087050,07091200,07094500,07099970,07099973,07109500,07124000,07130500,07133000,07134180&parameterCd=00064,00065,00010')
-  .then(function (usgsPayload) {
-    var basePayload = usgsPayload.data.value.timeSeries[0].sourceInfo;
-      for (var stationData in basePayload) {
-      console.log(basePayload[stationData].geogLocation);
+  request('http://waterservices.usgs.gov/nwis/iv/?format=json&sites=07079300,07081200,07083710,07087050,07091200,07094500,07099970,07099973,07109500,07124000,07130500,07133000,07134180&parameterCd=00060', (err, res, body) => {
+    if(!err && res.statusCode == 200) {
+      const usgsPayload = JSON.parse(body);
+      const parsedUSGS = usgsPayload.value.timeSeries;
+
+      for (var i = 0; i < parsedUSGS.length; i++) {
+        var stationData = {
+          river: 'Arkansas River',
+          site_name: parsedUSGS[i].sourceInfo.siteName,
+          flow_rate: parsedUSGS[i].values[0].value[0].value,
+          lat: parsedUSGS[i].sourceInfo.geoLocation.geogLocation.latitude,
+          lon: parsedUSGS[i].sourceInfo.geoLocation.geogLocation.longitude,
+          reading_date_time: parsedUSGS[i].values[0].value[0].dateTime
+        }
+        queries.updateRiverData(stationData, function (req, res, next) {
+          if (err) {
+            var returnObject = {};
+            returnObject.message = err.message || 'Data not added';
+            res.render('error', returnObject);
+          } else {
+            let returnObject = {};
+            returnObject.message = 'Data succesfully added!';
+            // res.redirect('/index');
+          }
+        });
+      }
     }
-//push into new array, use .reduce to purge the undefined items
+  })
 
-// .geoLocation.geogLocation.latitude
-    // var riverData = {
-    //   waterTemp:
-    //   flowRate:
-    //   depth:
-    //   latitude: usgsPayload.data.value.timeSeries[0].sourceInfo.geoLocation.geogLocation.latitude,
-    //   longitude:
-    //   timeStamp:
-    // }
-
-    // renderObject.usgsPayload = usgsPayload.data;
-  });
 });
 
 module.exports = router;
