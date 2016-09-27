6d0a20a1f0ff18064f9a1524cc502d7778f464d8
Adam Franzen <adamfranzen@gmail.com>
2016-09-27 08:38:47 -0600

    GMaps initializing in single river-rough draft!

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,38 @@
 (function () {
-  
+
+
+
 })();
+
+$(document).ready(function() {
+  addGoogleMapsScript();
+
+
+
+});
+
+function addGoogleMapsScript () {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src  = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDzaOBIjmRJeiSfhiXhPC4Wo4syHsQG_hc&callback=initMap&libraries=geometry';
+  $('head').append(s);
+}
+
+function newMap (lat, lng, map) {
+  console.log('newMap');
+  myLatLng = new google.maps.LatLng(lat, lng);
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: myLatLng
+  });
+  return map;
+}
+
+function initMap (lat, lng, map) {
+  console.log('initMap');
+  var lat = $(".geo").attr("data-lat")
+  var lng = $(".geo").attr("data-lon")
+  var myLatLng = new google.maps.LatLng(lat, lng);
+  var map = newMap(lat,lng);
+
+}
