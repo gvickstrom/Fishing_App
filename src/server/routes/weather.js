05041b45bb411f6c05629c2f0cf3548a00e89f9f
Adam Franzen <adamfranzen@gmail.com>
2016-09-28 15:19:27 -0600

    exported queries from index.js

---
+++ src/server/routes/weather.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+exports.getWeather = function (lat, lng) {
+  return axios.get(`https://api.darksky.net/forecast/2e41cd367153b0382dd154001a4576fc/${lat},${lng}`);
+};
