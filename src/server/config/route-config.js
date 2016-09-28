8bdb3529b8ad8c65b14e0d515e9c91e8854a3af8
Brandon Blair <brandon.d.blair@gmail.com>
2016-09-28 10:55:44 -0600

    added bcrypt and validation for signup and login

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -7,6 +7,7 @@
     // *** routes *** //
     const routes = require('../routes/index');
     const landing = require('../routes/landing');
+    const login = require('../routes/login');
     const signUp = require('../routes/sign-up');
     const homepage = require('../routes/homepage');
     const sites = require('../routes/sites');
@@ -14,6 +15,7 @@
     // *** register routes *** //
     app.use('/', routes);
     app.use('/landing', landing);
+    app.use('/login', login);
     app.use('/sign-up', signUp);
     app.use('/homepage', homepage);
     app.use('/sites', sites);
--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex.js');
-const validation = require('../../client/js/validation');
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
 
-router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
+  let loginName = req.body.login_username;
+  let loginPassword = req.body.login_password;
 
+  knex('users').select()
+  .then((data) => {
+    const passCompare = bcrypt.compareSync(loginPassword, data[0].password)
+    if (data[0].username === loginName && passCompare === true) {
+      res.redirect('/homepage');
+    }
+    else {
+      let msg = 'Incorrect username or password';
+      res.redirect('error');
+    }
+  })
 });
 
 module.exports = router;
