46a7ee3c0c890039fd3e8cb5ecf9498c031dedc8
Adam Franzen <adamfranzen@gmail.com>
2016-09-29 14:42:35 -0600

    single route working....for right now

--- src/server/routes/weather.js
+++ src/server/routes/weather.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
 exports.getWeather = function (lat, lng) {
+  console.log('hitWeather');
   return axios.get(`https://api.darksky.net/forecast/2e41cd367153b0382dd154001a4576fc/${lat},${lng}`);
 };
